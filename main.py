84f53223c42c5cb897caef42a742237529c1b37f
neoking89 <v.ouwendijk@gmail.com>
2024-09-08 01:24:38 +0200

    Refactor code structure and add TextGenerationProcessorTransformers

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import os
 import tempfile
 
 from src.utils.deep_learning import check_gpu_and_cuda
-from src.processors.onnx import TextGenerationProcessorOnnx
+from src.processors.text_generation import (
+    TextGenerationProcessorOnnx,
+    TextGenerationProcessorTransformers,
+)
 from src.utils.custom_classes import StateManager
 from src.utils.logger_manager import LoggerManager
 from src.utils.venv_manager import create_venv
@@ -26,18 +29,21 @@ def main() -> None:
     -------
     None
     """
-    model_path = os.environ.get(
-        "MODEL_PATH", r"models\small\cuda\cuda-int4-rtn-block-32"
+    model_path = r"models\small\cuda\cuda-int4-rtn-block-32"
+    # model_id = "TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF"
+    # filename = "tinyllama-1.1b-chat-v1.0.Q6_K.gguf"
+    processor = TextGenerationProcessorOnnx(
+        model_id=model_path, verbose=True, save_history=False
     )
+    # processor = TextGenerationProcessorTransformers(model_id=model_id, quantization_bits=None, save_history=True, gguf_file=filename)
+
     max_retries = 3
     max_new_tokens = 2048
 
     venv_dir = "venv"
     state_manager = StateManager()
 
-    with TextGenerationProcessorOnnx(
-        model_path=model_path, verbose=True, save_history=False
-    ) as processor, tempfile.TemporaryDirectory() as temp_dir, create_venv(
+    with tempfile.TemporaryDirectory() as temp_dir, create_venv(
         os.path.join(temp_dir, venv_dir)
     ) as pip_path:
         venv_path = os.path.join(temp_dir, venv_dir)
@@ -55,28 +61,16 @@ def main() -> None:
                 code_input = input(
                     "Enter Python code to execute using current state:\n"
                 )
-                code_executor.execute_and_retry(
-                    "python", code_input, "Direct Python input"
-                )
+                code_executor.execute_code_block("python", code_input)
             elif question.strip().lower() == "#clear":
-                state_manager.clear()
+                state_manager.clear_state()
                 processor.history.clear()
                 logger.info("State and history cleared.")
             else:
                 response = processor.generate(
                     question, max_new_tokens=max_new_tokens, stopwords=["```\n"]
                 )
-                execute_code_with_feedback(
-                    response,
-                    processor,
-                    question,
-                    max_retries,
-                    max_new_tokens,
-                    venv_path,
-                    pip_path,
-                    state_manager,
-                )
-
+                execute_code_with_feedback(response, question, code_executor)
 
 if __name__ == "__main__":
     main()
