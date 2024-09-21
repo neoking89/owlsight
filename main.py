14e4c8418f68e9a7210d8d9e6c36c4a282aeca83
neoking89 <v.ouwendijk@gmail.com>
2024-09-21 23:11:02 +0200

    feat: model selecting now inside application instead of python script

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from src.main_logic.main_loop import main
 from src.utils.deep_learning import check_gpu_and_cuda
-from src.processors.text_generation_processor import (
-    TextGenerationProcessorOnnx,
-    TextGenerationProcessorTransformers,
-)
 from src.processors.text_generation_manager import TextGenerationManager
 from src.ui.logo import print_logo
 from src.configurations.config_manager import ConfigManager
 
 
 from src.utils.logger_manager import LoggerManager
+
 logger = LoggerManager.get_logger(__name__)
 
 
@@ -17,15 +14,8 @@ if __name__ == "__main__":
     print_logo()
     check_gpu_and_cuda()
 
-    model_path = r"models\small\cuda\cuda-int4-rtn-block-32"
-    tokenizer = "microsoft/Phi-3-mini-4k-instruct"
-
     config_manager = ConfigManager()
-    config_manager.set("model.model_id", model_path)
-    config_manager.set("model.tokenizer", tokenizer)
-
     text_generation_manager = TextGenerationManager(
-        processor=TextGenerationProcessorOnnx,
         config_manager=config_manager,
     )
 
