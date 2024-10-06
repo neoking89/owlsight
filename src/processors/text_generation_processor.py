96900d5afc7abb22b7ad3e71afc124219162193e
neoking89 <v.ouwendijk@gmail.com>
2024-10-06 14:30:48 +0200

    feat: implement TextGenerationProcessorGGUF

--- src/processors/text_generation_processor.py
+++ src/processors/text_generation_processor.py
@@ -2,6 +2,7 @@ from abc import ABC
 from typing import Optional, List, Dict, Any, Type
 import os
 import time
+import traceback
 
 import torch
 from transformers import (
@@ -24,27 +25,61 @@ ONNX_MSG = "ONNX Runtime is disabled. Use 'pip install owlsight[onnx]' or instal
 try:
     import onnxruntime_genai as og
 except ImportError:
-    logger.warning(ONNX_MSG)
+    logger.warning("Support for ONNX models is disabled.")
     og = None
 
+try:
+    from llama_cpp import Llama
+except ImportError:
+    logger.warning(
+        "llama-cpp not found. Install it using 'pip install llama-cpp-python'."
+    )
+    Llama = None
+
+
+import os
+from typing import Type
 
 def select_processor_type(model_id: str) -> Type["TextGenerationProcessor"]:
     """
-    Select the appropriate text generation processor based on the model ID.
+    Select the appropriate text generation processor based on the model ID or directory.
+    
+    If the model_id is a directory, the function will inspect the contents of the directory
+    to decide the processor type. Otherwise, it will use the model_id string to make the decision.
     """
-    # first decide if model_id exists locally
-    if os.path.exists(model_id):
-        if not os.path.isdir(model_id):
-            raise NotADirectoryError(f"{model_id} is not a valid directory.")
-        if any([f.endswith(".onnx") for f in os.listdir(model_id)]):
+    # Check if the model_id is a directory
+    if os.path.isdir(model_id):
+        # Check if any file in the directory ends with .onnx
+        if any(f.endswith("onnx") for f in os.listdir(model_id)):
             return TextGenerationProcessorOnnx
+        elif any(f.endswith("gguf") for f in os.listdir(model_id)):
+            return TextGenerationProcessorGGUF
         else:
             return TextGenerationProcessorTransformers
     else:
-        if not "onnx" in model_id:
-            return TextGenerationProcessorTransformers
-        else:
+        # If model_id is not a directory, use the model_id string
+        if model_id.lower().endswith("gguf"):
+            return TextGenerationProcessorGGUF
+        elif "onnx" in model_id:
             return TextGenerationProcessorOnnx
+        else:
+            return TextGenerationProcessorTransformers
+
+    # if os.path.exists(model_id):
+    #     if not os.path.isdir(model_id):
+    #         if model_id.lower().endswith(".gguf"):
+    #             return TextGenerationProcessorGGUF
+    #         else:
+    #             raise ValueError(f"Unsupported file format: {model_id}")
+    #     if any([f.endswith(".onnx") for f in os.listdir(model_id)]):
+    #         return TextGenerationProcessorOnnx
+    #     else:
+    #         return TextGenerationProcessorTransformers
+    # else:
+    #     if "onnx" in model_id:
+    #         return TextGenerationProcessorOnnx
+    #     else:
+    #         return TextGenerationProcessorTransformers
 
 
 def flash_attention_is_available() -> bool:
@@ -65,7 +100,7 @@ class TextGenerationProcessor(ABC):
         ----------
         model_id: str
             The model ID to use for generation.
-            Uusally the name of the model or the path to the model.
+            Usually the name of the model or the path to the model.
         save_history : bool
             Whether or not to save the history of inputs and outputs.
         """
@@ -78,20 +113,18 @@ class TextGenerationProcessor(ABC):
         self.system_prompt = system_prompt
 
     def apply_chat_template(
-            self,
-            input_text: str,
-            tokenizer: PreTrainedTokenizer,
-        ) -> str:
+        self,
+        input_text: str,
+        tokenizer: PreTrainedTokenizer,
+    ) -> str:
         """
         Apply chat template to the input text.
         This is used to format the input text before generating a response and should be universal across all models.
         """
         if tokenizer.chat_template is not None:
             messages = []
-            # Include conversation history if save_history is enabled
             if self.save_history:
                 messages = self.history.copy()
-            # Add the user input and optionally the system prompt to the message list
             messages.append({"role": "user", "content": input_text})
             if self.system_prompt:
                 messages.insert(0, {"role": "system", "content": self.system_prompt})
@@ -100,9 +133,11 @@ class TextGenerationProcessor(ABC):
                 messages, tokenize=False, add_generation_prompt=True
             )
         else:
-            logger.warning("Chat template not found in tokenizer. Using input text as is.")
+            logger.warning(
+                "Chat template not found in tokenizer. Using input text as is."
+            )
             templated_text = input_text
-        
+
         return templated_text
 
     def update_history(self, input_text: str, generated_text: str):
@@ -125,7 +160,6 @@ class TextGenerationProcessorTransformers(TextGenerationProcessor):
         model_id: str,
         transformers__device: str = None,
         transformers__quantization_bits: Optional[int] = None,
-        transformers__gguf_file: Optional[str] = None,
         bnb_kwargs: Optional[dict] = None,
         tokenizer_kwargs: Optional[dict] = None,
         model_kwargs: Optional[dict] = None,
@@ -133,39 +167,6 @@ class TextGenerationProcessorTransformers(TextGenerationProcessor):
         system_prompt: str = "",
         **kwargs,
     ):
-        """
-        Text generation processor using Hugging Face Transformers library.
-
-        Parameters
-        ----------
-        model_id : str
-            The model ID to use for generation.
-            Usually the name of the model or the path to the model.
-        transformers__device : str, optional
-            The device to use for generation (default is "cuda" if available).
-        transformers__quantization_bits : int, optional
-            Number of quantization bits to use for the model.
-            Available options: 4, 8, None (default is None).
-        bnb_kwargs : dict, optional
-            Additional keyword arguments for BitsAndBytesConfig.
-        transformers__gguf_file : str, optional
-            Path to the GGUF file. Experimental feature.
-
-            See: https://huggingface.co/docs/transformers/en/gguf
-
-            Example Use:
-            >>> from transformers import AutoTokenizer, AutoModelForCausalLM
-            >>> model_id = "TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF"
-            >>> gguf_file = "tinyllama-1.1b-chat-v1.0.Q6_K.gguf"
-
-            >>> TextGenerationProcessorTransformers(model_id, transformers__gguf_file=gguf_file)
-        tokenizer_kwargs : dict, optional
-            Additional keyword arguments for the tokenizer.
-        model_kwargs : dict, optional
-            Additional keyword arguments for the model.
-        save_history : bool
-            Set to True if you want model to generate responses based on previous inputs.
-        """
         super().__init__(model_id, save_history, system_prompt, **kwargs)
         self.transformers__device = transformers__device or (
             "cuda" if torch.cuda.is_available() else "cpu"
@@ -177,7 +178,6 @@ class TextGenerationProcessorTransformers(TextGenerationProcessor):
 
         tokenizer, model = self._load_tokenizer_model(
             transformers__quantization_bits,
-            gguf_file=transformers__gguf_file,
             tokenizer_kwargs=tokenizer_kwargs or {},
             bnb_kwargs=bnb_kwargs or {},
             model_kwargs=model_kwargs or {},
@@ -195,7 +195,6 @@ class TextGenerationProcessorTransformers(TextGenerationProcessor):
     def _load_tokenizer_model(
         self,
         transformers__quantization_bits: Optional[int],
-        gguf_file: Optional[str],
         tokenizer_kwargs: Dict,
         bnb_kwargs: Dict,
         model_kwargs: Dict,
@@ -228,10 +227,6 @@ class TextGenerationProcessorTransformers(TextGenerationProcessor):
                 "_attn_implementation": self._attention_implementation,
             }
         )
-
-        if gguf_file:
-            tokenizer_kwargs["gguf_file"] = gguf_file
-            model_kwargs["gguf_file"] = gguf_file
 
         tokenizer = AutoTokenizer.from_pretrained(self.model_id, **tokenizer_kwargs)
         model = AutoModelForCausalLM.from_pretrained(self.model_id, **model_kwargs)
@@ -284,11 +279,7 @@ class TextGenerationProcessorTransformers(TextGenerationProcessor):
             generation_thread.kill()
             generation_thread.join()
 
-        if self.save_history:
-            self.history.append({"role": "user", "content": input_text})
-            self.history.append(
-                {"role": "assistant", "content": generated_text.strip()}
-            )
+        self.update_history(input_text, generated_text.strip())
 
         return generated_text
 
@@ -377,7 +368,9 @@ class TextGenerationProcessorOnnx(TextGenerationProcessor):
             Additional keyword arguments for generation.
             Example: {"top_k": 50, "top_p": 0.95}
         """
-        templated_text = self.apply_chat_template(input_text, self.transfomers_tokenizer)
+        templated_text = self.apply_chat_template(
+            input_text, self.transfomers_tokenizer
+        )
 
         search_options = {
             "do_sample": temperature > 0.0,
@@ -455,3 +448,79 @@ class TextGenerationProcessorOnnx(TextGenerationProcessor):
         self.tokenizer_stream = self.tokenizer.create_stream()
         logger.info(f"Model loaded using {self.onnx__num_threads} threads")
         logger.info("Tokenizer created")
+
+
+class TextGenerationProcessorGGUF(TextGenerationProcessor):
+    def __init__(
+        self,
+        model_id: str,
+        gguf__filename: str,
+        gguf__verbose: bool = False,
+        save_history: bool = False,
+        system_prompt: str = "",
+        **kwargs,
+    ):
+        super().__init__(model_id, save_history, system_prompt, **kwargs)
+
+        if Llama is None:
+            raise ImportError(
+                "llama-cpp not found. Install it using 'pip install llama-cpp-python'."
+            )
+
+        self.llm = Llama.from_pretrained(
+            repo_id=model_id,
+            filename=gguf__filename,
+            verbose=gguf__verbose,
+        )
+
+    def generate(
+        self,
+        input_text: str,
+        max_new_tokens: int = 512,
+        temperature: float = 0.1,
+        stopwords: Optional[List[str]] = None,
+        generation_kwargs: Optional[Dict[str, Any]] = None,
+    ) -> str:
+        templated_text = self.apply_chat_template(input_text)
+
+        _generation_kwargs = {
+            "max_tokens": max_new_tokens,
+            "temperature": temperature,
+            "stream": True,
+        }
+
+        if stopwords:
+            _generation_kwargs["stop"] = stopwords
+
+        if generation_kwargs:
+            _generation_kwargs.update(generation_kwargs)
+
+        generated_text = ""
+
+        try:
+            output = self.llm.create_chat_completion(templated_text, **_generation_kwargs)
+            for item in output:
+                new_text = item["choices"][0]["delta"].get("content", "")
+                generated_text += new_text
+                print(new_text, end="", flush=True)
+        except KeyboardInterrupt:
+            logger.warning("Control+C pressed, aborting generation")
+        except Exception:
+            logger.error(f"Error occured during generation: \n{traceback.format_exc()}")
+        finally:
+            print()  # Print newline after generation is done
+
+        self.update_history(input_text, generated_text.strip())
+
+        return generated_text.strip()
+
+    # override the original apply_chat_template method
+    def apply_chat_template(self, input_text) -> List[Dict[str, str]]:
+        messages = []
+        if self.save_history:
+            messages = self.history.copy()
+        messages.append({"role": "user", "content": input_text})
+        if self.system_prompt:
+          messages.insert(0, {"role": "system", "content": self.system_prompt})
+  
+        return messages
