91fd1d380366e61bbe8e579efdec7e5479300f65
neoking89 <v.ouwendijk@gmail.com>
2024-10-08 19:35:32 +0200

    feat: add get_best_device

--- src/utils/deep_learning.py
+++ src/utils/deep_learning.py
@@ -17,8 +17,12 @@ def free_memory():
 
 def print_memory_stats(device: torch.device):
     """Print two different measures of GPU memory usage."""
-    print(f"Max memory allocated: {torch.cuda.max_memory_allocated(device) / 1e9:.2f} GB")
-    print(f"Max memory reserved: { torch.cuda.max_memory_reserved(device) / 1e9:.2f} GB")
+    print(
+        f"Max memory allocated: {torch.cuda.max_memory_allocated(device) / 1e9:.2f} GB"
+    )
+    print(
+        f"Max memory reserved: { torch.cuda.max_memory_reserved(device) / 1e9:.2f} GB"
+    )
 
 
 def calculate_model_size(model) -> float:
@@ -37,17 +41,23 @@ def check_gpu_and_cuda():
     if torch.cuda.is_available():
         gpu = torch.cuda.get_device_name(0)
         logger.info(f"GPU found: {gpu}")
-        logger.info("CUDA-capable GPU is available and PyTorch is built with CUDA support.")
+        logger.info(
+            "CUDA-capable GPU is available and PyTorch is built with CUDA support."
+        )
 
     cuda_version = None
     try:
         output_cuda = subprocess.check_output(["nvcc", "--version"]).decode("utf-8")
         cuda_version = output_cuda[
-            output_cuda.find("release") + len("release") + 1 : output_cuda.find(",", output_cuda.find("release"))
+            output_cuda.find("release")
+            + len("release")
+            + 1 : output_cuda.find(",", output_cuda.find("release"))
         ]
         logger.info("CUDA %s is installed.", cuda_version)
     except subprocess.CalledProcessError:
-        logger.warning("Warning: CUDA-capable GPU is available, but CUDA is not installed. Please install CUDA.")
+        logger.warning(
+            "Warning: CUDA-capable GPU is available, but CUDA is not installed. Please install CUDA."
+        )
     except Exception as e:
         logger.error("%s", e)
 
@@ -56,7 +66,7 @@ def check_gpu_and_cuda():
     else:
         logger.warning(
             "Cuda is currently unavailable. This could be expected if no GPU is available. If not, please visit 'https://pytorch.org/get-started/locally/' to install a compatible version.\nrun command 'pip uninstall torch torchvision torchaudio' and find run the right version of PyTorch for your CUDA version.",
-         )
+        )
 
 
 def log_reserved_memory():
@@ -113,7 +123,9 @@ def calculate_available_vram() -> float:
     Calculate the available VRAM on the GPU in GB.
     """
     if torch.cuda.is_available():
-        available_memory = torch.cuda.get_device_properties(0).total_memory - torch.cuda.memory_allocated(0)
+        available_memory = torch.cuda.get_device_properties(
+            0
+        ).total_memory - torch.cuda.memory_allocated(0)
         available_memory_gb = available_memory / 1024**3
         return available_memory_gb
     else:
@@ -131,7 +143,23 @@ def calculate_max_parameters_per_dtype():
         logger.info(f"Available VRAM: {available_vram:.2f} GB")
 
         for bits in [32, 16, 8, 4]:
-            max_params = available_vram / calculate_memory_for_model(1, bits)  # for 1 billion parameters
-            logger.info(f"Maximum number of billion parameters for {bits}-bit model: {max_params:.2f} billion")
+            max_params = available_vram / calculate_memory_for_model(
+                1, bits
+            )  # for 1 billion parameters
+            logger.info(
+                f"Maximum number of billion parameters for {bits}-bit model: {max_params:.2f} billion"
+            )
     else:
         logger.warning("No available VRAM to calculate parameters.")
+
+
+def get_best_device() -> str:
+    """
+    Check for best device and return the device name.
+    """
+    if torch.cuda.is_available():
+        return "cuda"
+    elif torch.backends.mps.is_available():
+        return "mps"
+    else:
+        return "cpu"
