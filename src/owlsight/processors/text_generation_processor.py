e4dd2d0765590b32486403a5dd591a3aa90eecac
neoking89 <v.ouwendijk@gmail.com>
2024-10-20 18:57:43 +0200

    feat: implement get_history

--- src/owlsight/processors/text_generation_processor.py
+++ src/owlsight/processors/text_generation_processor.py
@@ -110,13 +110,8 @@ class TextGenerationProcessor(ABC):
         This is used to format the input text before generating a response and should be universal across all models.
         """
         if tokenizer.chat_template is not None:
-            messages = []
-            if self.save_history:
-                messages = self.history.copy()
+            messages = self.get_history()
             messages.append({"role": "user", "content": input_text})
-            if self.system_prompt:
-                messages.insert(0, {"role": "system", "content": self.system_prompt})
-
             templated_text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
         else:
             logger.warning("Chat template not found in tokenizer. Using input text as is.")
@@ -129,6 +124,14 @@ class TextGenerationProcessor(ABC):
         if self.save_history:
             self.history.append({"role": "user", "content": input_text})
             self.history.append({"role": "assistant", "content": generated_text.strip()})
+
+    def get_history(self) -> List[Dict[str, str]]:
+        """Get complete chathistory of inputs and outputs and system prompt."""
+        messages = self.history.copy()
+        if self.system_prompt:
+            messages.insert(0, {"role": "system", "content": self.system_prompt})
+
+        return messages
 
     def generate(self) -> str:
         raise NotImplementedError("generate method must be implemented in the subclass.")
