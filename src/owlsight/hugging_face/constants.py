b483abfa9ecd2345b2356786b3046f86c8155f7c
neoking89 <v.ouwendijk@gmail.com>
2024-11-14 00:11:54 +0100

    fix: add TASK_TO_AUTO_MODEL mapping from SUPPORTED_TASKS for model retrieval

--- src/owlsight/hugging_face/constants.py
+++ src/owlsight/hugging_face/constants.py
@@ -1,4 +1,8 @@
-# from transformers.pipelines import SUPPORTED_TASKS
+from transformers.pipelines import SUPPORTED_TASKS
+
+TASK_TO_AUTO_MODEL = {
+    k:v["pt"][0] for k,v in SUPPORTED_TASKS.items()
+}
 
 HUGGINGFACE_TASKS = [None] + [
     "text-generation",
