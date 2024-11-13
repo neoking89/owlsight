99840ba093ce849f304d931c227ae62d87fbe12e
neoking89 <v.ouwendijk@gmail.com>
2024-11-13 19:46:16 +0100

    fix: limit the available tasks

--- src/owlsight/hugging_face/constants.py
+++ src/owlsight/hugging_face/constants.py
@@ -1,13 +1,7 @@
-from transformers.pipelines import SUPPORTED_TASKS
+# from transformers.pipelines import SUPPORTED_TASKS
 
-EXCLUDED_TASKS = [
-    "feature-extraction",
-    "table-question-answering",
-    "zero-shot-classification",
-    "zero-shot-image-classification",
-    "zero-shot-audio-classification",
-    "conversational",
-    "zero-shot-object-detection",
+HUGGINGFACE_TASKS = [None] + [
+    "text-generation",
+    "translation",
+    "summarization",
 ]
-
-HUGGINGFACE_TASKS = [None] + [task for task in SUPPORTED_TASKS if task not in EXCLUDED_TASKS]
