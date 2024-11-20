72da572d10055553671e1c1ce46d6ec8f43dde2b
neoking89 <v.ouwendijk@gmail.com>
2024-11-20 01:03:30 +0100

    feat: extend HUGGINGFACE_TASKS to include media-related tasks in constants.py

--- src/owlsight/hugging_face/constants.py
+++ src/owlsight/hugging_face/constants.py
@@ -1,16 +1,27 @@
 from transformers.pipelines import SUPPORTED_TASKS
+from transformers import QuestionAnsweringPipeline, DocumentQuestionAnsweringPipeline,  AutomaticSpeechRecognitionPipeline, VisualQuestionAnsweringPipeline, TranslationPipeline, SummarizationPipeline
 
-TASK_TO_AUTO_MODEL = {
-    k:v["pt"][0] for k,v in SUPPORTED_TASKS.items()
-}
+TASK_TO_AUTO_MODEL = {k: v["pt"][0] for k, v in SUPPORTED_TASKS.items()}
 
 HUGGINGFACE_TASKS = [None] + [
     "text-generation",
     "text2text-generation",
     "translation",
     "summarization",
+]
+
+HUGGINGFACE_MEDIA_TASKS = [
     "image-to-text",
     "automatic-speech-recognition",
     "visual-question-answering",
-    "document-question-answering"
+    "document-question-answering",
 ]
+
+# MEDIA_PIPELINE_MAPPING = {
+#     "image-to-text": "image",
+#     "automatic-speech-recognition": "audio",
+#     "visual-question-answering": "image",
+#     "document-question-answering": "image",
+# }
+
+HUGGINGFACE_TASKS.extend(HUGGINGFACE_MEDIA_TASKS)
