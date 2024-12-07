f08c6d4a737c179a1372ed016cdc98ecbab6d53c
neoking89 <v.ouwendijk@gmail.com>
2024-12-07 02:00:26 +0100

    test: add unit tests for parse_media_placeholders function

--- tests/unit_tests/test_replace_bracket_placeholders.py
+++ tests/unit_tests/test_replace_bracket_placeholders.py
@@ -7,7 +7,7 @@ sys.path.append("src")
 
 import pytest
 
-from owlsight.utils.helper_functions import replace_bracket_placeholders
+from owlsight.utils.helper_functions import replace_bracket_placeholders, parse_media_placeholders
 
 
 # Fixture for test data
@@ -50,7 +50,7 @@ def test_basic_expressions(test_context, test_id, input_string, expected):
     [
         ("struct_1", "{{{1, 2, 3}}}", {1, 2, 3}),
         ("struct_2", "{{[x for x in range(3)]}}", [0, 1, 2]),
-            ("struct_3", "{{{'key': 5}}}", {"key": 5}),
+        ("struct_3", "{{{'key': 5}}}", {"key": 5}),
         ("struct_4", "{{(1, 2, 3)}}", (1, 2, 3)),
     ],
 )
@@ -76,7 +76,7 @@ def test_string_operations(test_context, test_id, input_string, expected):
 @pytest.mark.parametrize(
     "test_id, input_string, expected",
     [
-        ("complex_1", '{{5*2}} is more than {{2*3}}', "10 is more than 6"),
+        ("complex_1", "{{5*2}} is more than {{2*3}}", "10 is more than 6"),
         ("complex_2", '{{" ".join([str(x) for x in range(3)])}}', "0 1 2"),
     ],
 )
@@ -140,6 +140,156 @@ def test_error_handling(test_id, input_string, expected_error):
     with pytest.raises(expected_error):
         replace_bracket_placeholders(input_string, {})
 
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-v"])
+
+
+# Add to test_helper_functions.py
+
+import pytest
+from owlsight.utils.helper_functions import parse_media_placeholders
+
+
+# Basic media syntax
+@pytest.mark.parametrize(
+    "test_id, input_string, var_dict, expected_text, expected_media",
+    [
+        (
+            "basic_image",
+            "[[image:photo.jpg]]",
+            {},
+            "__MEDIA_0__",
+            {"__MEDIA_0__": {"type": "image", "path": "photo.jpg", "options": {}}},
+        ),
+        (
+            "basic_audio",
+            "[[audio:recording.mp3]]",
+            {},
+            "__MEDIA_0__",
+            {"__MEDIA_0__": {"type": "audio", "path": "recording.mp3", "options": {}}},
+        ),
+        (
+            "basic_video",
+            "[[video:clip.mp4]]",
+            {},
+            "__MEDIA_0__",
+            {"__MEDIA_0__": {"type": "video", "path": "clip.mp4", "options": {}}},
+        ),
+    ],
+)
+def test_basic_media_syntax(test_id, input_string, var_dict, expected_text, expected_media):
+    result_text, result_media = parse_media_placeholders(input_string, var_dict)
+    assert result_text == expected_text
+    assert result_media == expected_media
+
+
+# Python expression integration
+@pytest.mark.parametrize(
+    "test_id, input_string, var_dict, expected_text, expected_media",
+    [
+        (
+            "python_expr",
+            "[[image:{{folder}}/{{filename}}]]",
+            {"folder": "photos", "filename": "cat.jpg"},
+            "__MEDIA_0__",
+            {"__MEDIA_0__": {"type": "image", "path": "photos/cat.jpg", "options": {}}},
+        ),
+        (
+            "python_expr_options",
+            "[[image:{{folder}}/test.jpg|width={{size}}]]",
+            {"folder": "images", "size": 512},
+            "__MEDIA_0__",
+            {"__MEDIA_0__": {"type": "image", "path": "images/test.jpg", "options": {"width": "512"}}},
+        ),
+    ],
+)
+def test_python_expression_integration(test_id, input_string, var_dict, expected_text, expected_media):
+    result_text, result_media = parse_media_placeholders(input_string, var_dict)
+    assert result_text == expected_text
+    assert result_media == expected_media
+
+
+# Multiple media objects and options
+@pytest.mark.parametrize(
+    "test_id, input_string, var_dict, expected_text, expected_media",
+    [
+        (
+            "multiple_media",
+            "Compare [[image:first.jpg]] with [[image:second.jpg]]",
+            {},
+            "Compare __MEDIA_0__ with __MEDIA_1__",
+            {
+                "__MEDIA_0__": {"type": "image", "path": "first.jpg", "options": {}},
+                "__MEDIA_1__": {"type": "image", "path": "second.jpg", "options": {}},
+            },
+        ),
+        (
+            "complex_options",
+            "[[image:photo.jpg|width=512|height=512|pipeline=depth-estimation]]",
+            {},
+            "__MEDIA_0__",
+            {
+                "__MEDIA_0__": {
+                    "type": "image",
+                    "path": "photo.jpg",
+                    "options": {"width": "512", "height": "512", "pipeline": "depth-estimation"},
+                }
+            },
+        ),
+    ],
+)
+def test_multiple_media_and_options(test_id, input_string, var_dict, expected_text, expected_media):
+    result_text, result_media = parse_media_placeholders(input_string, var_dict)
+    assert result_text == expected_text
+    assert result_media == expected_media
+
+
+# Mixed content
+@pytest.mark.parametrize(
+    "test_id, input_string, var_dict, expected_text, expected_media",
+    [
+        (
+            "mixed_content",
+            "The value is {{2 + 2}} and here's an [[image:test.jpg]]",
+            {},
+            "The value is 4 and here's an __MEDIA_0__",
+            {"__MEDIA_0__": {"type": "image", "path": "test.jpg", "options": {}}},
+        ),
+        (
+            "mixed_complex",
+            """Process this [[image:{{folder}}/{{filename}}|width={{size}}]] 
+            with value {{x + 1}} and [[audio:recording.mp3|language=en]]""",
+            {"folder": "imgs", "filename": "test.jpg", "size": 256, "x": 5},
+            """Process this __MEDIA_0__ 
+            with value 6 and __MEDIA_1__""",
+            {
+                "__MEDIA_0__": {"type": "image", "path": "imgs/test.jpg", "options": {"width": "256"}},
+                "__MEDIA_1__": {"type": "audio", "path": "recording.mp3", "options": {"language": "en"}},
+            },
+        ),
+    ],
+)
+def test_mixed_content(test_id, input_string, var_dict, expected_text, expected_media):
+    result_text, result_media = parse_media_placeholders(input_string, var_dict)
+    assert result_text == expected_text
+    assert result_media == expected_media
+
+
+# Error handling
+def test_invalid_media_type():
+    with pytest.raises(ValueError):
+        parse_media_placeholders("[[invalid:test.jpg]]", {})
+
+
+def test_missing_path():
+    with pytest.raises(ValueError):
+        parse_media_placeholders("[[image:]]", {})
+
+
+def test_invalid_option_format():
+    with pytest.raises(ValueError):
+        parse_media_placeholders("[[image:test.jpg|invalid_option]]", {})
 
 if __name__ == "__main__":
     pytest.main([__file__, "-v"])
