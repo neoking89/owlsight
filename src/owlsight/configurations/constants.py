0feff4e44d56fdfeef2c80d8a72cc3ca811402eb
neoking89 <v.ouwendijk@gmail.com>
2024-12-27 01:14:56 +0100

    fix: add constants module for configuration defaults and choices

---
+++ src/owlsight/configurations/constants.py
@@ -0,0 +1,7 @@
+from owlsight.configurations.schema import Schema
+
+
+CONFIG_DEFAULTS = Schema.get_config_defaults()
+CONFIG_CHOICES = Schema.get_config_choices()
+CONFIG_DESCRIPTIONS = Schema.get_config_descriptions()
+MAIN_MENU = Schema.get_main_menu()
