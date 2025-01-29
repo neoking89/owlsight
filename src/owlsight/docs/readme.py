6008a4a4576d86976a56cd6d92f8159c6ad3e66b
neoking89 <v.ouwendijk@gmail.com>
2025-01-29 01:32:09 +0100

    docs: add description for new owl_tools function in README

--- src/owlsight/docs/readme.py
+++ src/owlsight/docs/readme.py
@@ -370,7 +370,7 @@ The advantage is that `model_kwargs` can now also be passed inside `TextGenerati
 - ESC + V can be used inside the "How can I assist you?"-option after typing the following: "[[", "{{". This will autocomplete the following:
 "[[" will autocomplete to: "image:", "audio:"
 "{{" will autocomplete any available defined objects from the python-namespace.
-
+- Added `owl_tools` function to the Python interpreter. This function can be used to convert all defined functions in the namespace to a dictionary, which can be used for tool/function-calling.
 If you encounter any issues, feel free to shoot me an email at v.ouwendijk@gmail.com""".strip()
 
 
