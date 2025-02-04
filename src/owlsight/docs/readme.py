c182aece1275526a387ed463d1b925623ee259b9
neoking89 <v.ouwendijk@gmail.com>
2025-02-04 19:48:12 +0100

    refactor: add support for [[chain:...]] tag to modify config parameters during conversations

--- src/owlsight/docs/readme.py
+++ src/owlsight/docs/readme.py
@@ -402,6 +402,8 @@ The idea is that this might help the model to give a more focused response to th
 "[[load:config-to-model1.json]] Generates a rough draft for the following text: {{{{owl_read("mockup-idea.txt")}}}} [[load:config-to-model2.json]] Validate that the generated draft based on the previous text is relevant and contains all necessary information"
 TIP 1: Combing a sequence of different agents together with above method can lead to complex conversation flows.
 TIP 2: Using above tag in combination with `sequence_on_loading` in the configuration json opens lots of new possibilities to control the application.
+- Added `[[chain:...]]` tag support for changing config parameters in between conversations. For example: "[[chain:model.system_prompt=act as a helpful assistant||generate.temperature=0.5]]".
+
 
 If you encounter any issues, feel free to shoot me an email at v.ouwendijk@gmail.com""".strip()
 
